d67fb3cd5bdcae52cd73b5cb81a11d4d7ef45f22
Tommy Menheere <github@tommym.net>
2014-12-03 00:02:27 +0100

    updated

--- TimeLapse.py
+++ TimeLapse.py
@@ -6,7 +6,6 @@ import os.path
 import glob
 import signal
 import sys
-import RPi.GPIO as GPIO
 import random
 import PIL
 import io
@@ -16,12 +15,11 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-
 camera = picamera.PiCamera()
 config = ConfigParser.ConfigParser()
 sequence = 0
 
-default_resolution = (2592, 1944)
+default_resolution = (1920, 1080)
 
 def signal_handler(signal, frame):
         print("Exit")
@@ -30,8 +28,6 @@ def signal_handler(signal, frame):
 def init():
 	global sequence
 	global interval
-	
-	config.read("./config.ini")
 	interval = int(config.get('capture', 'interval'))
 	signal.signal(signal.SIGINT, signal_handler)
 	root = get_root();
@@ -45,13 +41,15 @@ def init():
 			filename = os.path.basename(file);
 			if filename > newest:
 				newest = filename;
-		sequence = int(newest[:5]) + 1 
-
+		sequence = int(newest[:5]) 
+		
 def get_root():
+	global sequence
 	root = time.strftime(config.get('files', 'root'), time.localtime())
 	if not os.path.exists(root):
 		print "Root does not exist ({}), creating it ...".format(root)
 		os.makedirs(root)
+		sequence = 0;
 	return root
 
 def capture():
@@ -59,27 +57,41 @@ def capture():
 	sequence += 1
 	root = get_root()
 	path = "%s/%05d.%s"  % (root, sequence, config.get('capture', 'format'))
-	print "Capturing to %s" % path
+	#print "Capturing to %s" % path
 	
 	if config.getboolean('overlay', 'enable'):
 		stream = io.BytesIO()
 		camera.capture( stream, format=config.get('capture', 'format') )
 		final_overlay_text = time.strftime(config.get('overlay', 'text'), time.localtime())
 		stream.seek(0)
-		
 		image = Image.open(stream)
 		draw = ImageDraw.Draw(image)
 		font = ImageFont.truetype(config.get('overlay', 'font_name'), int(config.get('overlay', 'font_size')))
-		print "Added overlay " + final_overlay_text
+		#print "Added overlay " + final_overlay_text
 		draw.text((int(config.get('overlay', 'offset_x')), int(config.get('overlay', 'offset_y')) ), final_overlay_text, config.get('overlay', 'font_color'),font=font)
 		draw = ImageDraw.Draw(image)
 		image.save(path)
+		
 	else:
 		camera.capture( path, format=config.get('capture', 'format') )
-	print "Wrote file %s" % path
+		
+	#print "Wrote file %s" % path
+
+
+def read_config( file ):
+	if os.path.isfile( file ):
+		config.read(file)
+		print "Reading configuration from %s" % file
+	else:
+		print "Unable to find file %s" % file
+		sys.exit(1)
 
 def main():
 	try:
+		if len(sys.argv) == 2:
+			read_config(sys.argv[1])
+		else:
+			read_config("./config.ini")
 		init()
 		camera.vflip = config.getboolean('capture', 'vertical_flip')
 		camera.hflip = config.getboolean('capture', 'horizontal_flip')
@@ -91,11 +103,10 @@ def main():
 			print "Using default resolution (%dx%d)" % default_resolution
 #		time.sleep(2)
 		camera.start_preview()
-		print "Started"
+		print "Using interval %ds" % interval
 		rt = RepeatedTimer(interval, capture)
 		while (True):
 			time.sleep(60)
-			print "Alive"
 	finally:
 		camera.stop_preview()
 		camera.close()
